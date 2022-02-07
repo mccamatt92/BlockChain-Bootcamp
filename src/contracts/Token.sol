82fb64fbc56d5cc2f4fda53041a612b565a9aaad
Matthias McCarthy <matthias.mccarthy@gmail.com>
2022-02-07 19:05:34 +0100

    Created My token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,5 +2,12 @@ pragma solidity ^0.5.16;
 
 contract Token {
   string public name ="Matt Token";   
+  string public symbol = "MATT";
+  uint256 public decimals = 18;
+  uint256 public totalSupply;
+
+  constructor() public {
+    totalSupply  = 100000 * (10 ** decimals);
+  }
 }
 
